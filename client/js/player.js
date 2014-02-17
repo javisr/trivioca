a9fb6c7a51eb1d23407a91197ec37420d65eea8c
Javisr <javi@javsr.net>
2014-02-17 23:29:39 +0100

    @javisr Description of player.js

--- client/js/player.js
+++ client/js/player.js
@@ -1,41 +1,66 @@
 Player = (function () {
-    var box, dice, waitingTurns;
 
+    var dice; //dado
+    var waitingTurns; //Turnos que tiene de penalización el player
+    var lastBox; //última casilla del tablero
+    var currentBox; //casilla actual
+    var name; //nombre del player
+
+    /*
+    *  Este es el constructor de la clase Player.
+    *  - name: nombre del jugador.
+    *  - totalBox: número total de casillas del tablero.
+     */
     function Player(name, totalBox) {
-        var lastBox;
         this.name = name;
-        lastBox = totalBox;
+        this.lastBox = totalBox;
     }
 
-    box = 0;
+    currentBox = 0;
 
     waitingTurns = 0;
 
     dice = new Dice();
 
+    /*
+    * Mover el jugador a la casilla X
+    * - newBow: nueva casilla
+     */
     Player.prototype.moveToBox = function (newBox) {
-        return box = newBox;
+        //TODO cotrol that newBox is a int value;
+        return currentBox = newBox;
     };
 
-    Player.prototype.currentBox = function () {
-        return box;
+    /*
+     * currentBox getter
+     */
+    Player.prototype.getCurrentBox = function () {
+        return currentBox;
     };
 
+    /*
+    * waitingTurns setter
+     */
     Player.prototype.setWaitingTurns = function (turn) {
         return waitingTurns = turn;
     };
 
+    /*
+    * tiradas
+     */
     Player.prototype.turn = function () {
-        var returned;
-        returned = {};
-        if (waitingTurns === 0) {
-            box += dice["throw"]();
-            if (box > lastBox) {
-                box -= box - lastBox;
+        var returned = {};
+        if (waitingTurns === 0) { //si no tiene turnos de espera
+            currentBox += dice["throw"]();
+
+            if (currentBox > lastBox) {
+                currentBox -= currentBox - lastBox;
             }
+            //TODO poner una animación?
+            
             returned = {
-                'next': false,
-                'box': box
+                'next': false, //False para que no salte ya al siguiente jugador
+                'box': currentBox //casilla nueva.
             };
         } else {
             waitingTurns--;
