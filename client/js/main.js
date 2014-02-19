14e941f783708ec7e5e2250478487304deccf0f8
Javisr <javi@javsr.net>
2014-02-19 22:00:39 +0100

    deleting unnecessary files

--- client/js/main.js
+++
@@ -1,7 +0,0 @@
-boardApp = new BoardApp(boardData);
-
-$('#questionWrapper').on('click', '.answer', function (event) {
-    event.preventDefault();
-    alert($(this).data('valid'));
-    return $(this).closest("#questionWrapper").html('');
-});
